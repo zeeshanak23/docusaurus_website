df6649be2ca8df125b11990d4958fd16245409ef
zeeshanak23 <zakhter449@gmail.com>
2023-07-29 19:25:42 +0530

    added vercel file

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,6 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
+  distDir: 'build',
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
